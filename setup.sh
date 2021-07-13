06623b6c082f6ca897c87adf460d52996a65267d
agb1986 <abrolly1986@gmail.com>
2021-07-13 21:28:52 +0100

    Adding setyup script and postman collection

---
+++ setup.sh
@@ -0,0 +1,11 @@
+echo 'Creating venv'
+python -m venv ./.env;
+
+echo 'Starting venv'
+source ./.env/Scripts/activate;
+
+echo 'Pulling dependencies'
+pip install -r requirements.txt;
+
+echo 'Starting Flask server'
+flask run | pytest test.py;
